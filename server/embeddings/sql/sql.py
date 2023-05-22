f125fd499930fa4adcb70623302b66bf7bd67c58
christianwengert <christian@codefour.ch>
2023-05-22 16:00:04 +0200

    add sql, change model, it seems stable-vicuna is not optimal

--- server/embeddings/sql/sql.py
+++ server/embeddings/sql/sql.py
@@ -2,7 +2,7 @@ import os
 from langchain import SQLDatabase, LlamaCpp, SQLDatabaseChain
 from models import MODEL_PATH, MODELS
 
-MODEL = 'stable-vicuna-13B.ggml.q5_0'
+MODEL = 'Wizard-Vicuna-7B-Uncensored.ggmlv3.q5_0'
 
 
 def qa_on_sql(connect_string: str, model: str):
@@ -11,22 +11,31 @@ def qa_on_sql(connect_string: str, model: str):
 
     db = SQLDatabase.from_uri(connect_string,
                               engine_args=dict(connect_args={"check_same_thread": False}, echo=True),
-                              # include_tables=['employees']
+                              include_tables=['employees', 'albums']
                               )
 
-    llm = LlamaCpp(model_path=os.path.join(MODEL_PATH, MODEL),
-                   temperature=0.8,
+    llm = LlamaCpp(model_path=os.path.join(MODEL_PATH, f'{model}.bin'),
+                   temperature=0.0,
                    n_threads=8,
                    n_ctx=n_ctx,
                    n_batch=512,
                    max_tokens=256,
                    )
 
-    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+    db_chain = SQLDatabaseChain.from_llm(llm, db,
+                                         verbose=True,
+                                         use_query_checker=True,
+                                         )
 
     answer = db_chain.run("How many employees are there?")
-
     print(answer)
+
+    answer = db_chain.run("How many albums by Aerosmith?")
+    print(answer)
+
+    answer = db_chain.run("What is the average salary?")
+    print(answer)
+
 
 
 if __name__ == '__main__':
