b91e32047138d49da406f7412db6b8221f24c8fe
christianwengert <christian@codefour.ch>
2024-04-10 11:37:02 +0200

    add command-r template

--- server/app.py
+++ server/app.py
@@ -473,6 +473,20 @@ def make_prompt(hist, system_prompt, text, prompt_template):
         prompt += f' [INST] {text} [/INST]'
         return prompt
 
+    if prompt_template == 'command-r':
+        # <BOS_TOKEN><|START_OF_TURN_TOKEN|><|USER_TOKEN|>{prompt}<|END_OF_TURN_TOKEN|><|START_OF_TURN_TOKEN|><|CHATBOT_TOKEN|>{response}
+        prompt = ''
+        prompt += f'<BOS_TOKEN><|START_OF_TURN_TOKEN|>'
+        prompt += f'<|USER_TOKEN|>{system_prompt}<|END_OF_TURN_TOKEN|>'
+        for line in hist['items']:
+            if line['role'] == USER:
+                prompt += f'<|START_OF_TURN_TOKEN|><|USER_TOKEN|>user\n{line["content"]}<|END_OF_TURN_TOKEN|>\n'
+            if line['role'] == ASSISTANT:
+                prompt += f'<|CHATBOT_TOKEN|>assistant\n{line["content"]}<|END_OF_TURN_TOKEN|>'
+        prompt += f'<|START_OF_TURN_TOKEN|>user\n{text}<|END_OF_TURN_TOKEN|>'
+        prompt += f'<|CHATBOT_TOKEN|>assistant'
+        return prompt
+
     if prompt_template == 'chatml':
         # <|im_start|>system
         # {system_message}<|im_end|>
